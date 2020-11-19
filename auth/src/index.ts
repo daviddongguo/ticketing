edbab99ea933a0a8378893b66c3cdbfd50320b77
daviddongguo <david.dong.guo@gmail.com>
2020-11-19 09:42:58 -0500

    Change auth server port from 3000 to 3018

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,7 +4,12 @@ import express from 'express';
 const app = express();
 app.use(json());
 
-app.get('/api/users/test', (req, res)=>{
+app.get('/test', (req, res)=>{
+  console.log(req.url);
+  console.log(req.route);
+  res.status(200).json({message: 'Hi, there!'});
+});
+app.get('/api/users/currentuser', (req, res)=>{
   console.log('/api/users/test received.')
   res.status(200).json({message: 'Hi, there!'});
 });
