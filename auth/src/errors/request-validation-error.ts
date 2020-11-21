5b848f649b7b267b140944519e818289d8a96de0
daviddongguo <david.dong.guo@gmail.com>
2020-11-21 14:05:43 -0500

    Upgrade request validation error

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -2,8 +2,8 @@ import {ValidationError} from 'express-validator';
 import {CustomError} from './custom-error';
 
 export class RequestValidationError extends CustomError {
-	constructor(public errors: ValidationError[]) {
-		super('Invalidate Inputs');
+	constructor(public errors: ValidationError[], message?: string) {
+		super(message || 'Invalid Inputs');
 		// Only because we are extending a built in class
 		Object.setPrototypeOf(this, RequestValidationError.prototype);
 	}
