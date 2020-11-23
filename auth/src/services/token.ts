76a4ca299f53a740c23e002e36a8af7f4da0c71d
daviddongguo <david.dong.guo@gmail.com>
2020-11-22 23:49:22 -0500

    Add verify token in token

--- auth/src/services/token.ts
+++ auth/src/services/token.ts
@@ -1,11 +1,29 @@
 import jwt from 'jsonwebtoken';
 import {UserDoc} from '../models/user';
 
-export const generateToken = (user: UserDoc) => {
-	return jwt.sign({
-    id: user.id,
-    email: user.email
-  },
-  process.env.JWT_KEY!
-  );
+const generateToken = (user: UserDoc) => {
+	return jwt.sign(
+		{
+			id: user.id,
+			email: user.email,
+		},
+		process.env.JWT_KEY!
+	);
 };
+
+const verifyToken = (token: string) => {
+  if(!token){
+    console.error('token is not provided!');
+    return null;
+  }
+
+	try {
+    return jwt.verify(token, process.env.JWT_KEY!);
+	} catch (error) {
+    console.error('token is not valid!');
+		return null;
+	}
+};
+
+export {generateToken, verifyToken};
+
