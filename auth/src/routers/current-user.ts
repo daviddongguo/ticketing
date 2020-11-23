c7e8ece4a068ee3f7a4050beb29ed943f08673e5
daviddongguo <david.dong.guo@gmail.com>
2020-11-22 23:49:44 -0500

    Finish currentuser

--- auth/src/routers/current-user.ts
+++ auth/src/routers/current-user.ts
@@ -1,9 +1,10 @@
 import express from 'express';
+import {verifyToken} from '../services/token';
 
 const router = express.Router();
 
 router.get('/api/users/currentuser', (req, res) => {
-	res.status(200).json({message: `${req.url}`});
+	return res.status(200).json({currentuser: verifyToken(req.session?.jwt)});
 });
 
 export {router as currentUserRouter};
