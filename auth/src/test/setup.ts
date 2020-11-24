b983dd43217123bdcd6619bdb57058c23e72d0b2
daviddongguo <david.dong.guo@gmail.com>
2020-11-23 21:26:14 -0500

    Add global method for sign up

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,29 +1,48 @@
 import {MongoMemoryServer} from 'mongodb-memory-server';
 import mongoose from 'mongoose';
+import request from 'supertest';
+import {app} from '../app';
+
+declare global {
+	namespace NodeJS {
+		interface Global {
+			signup(email: string): Promise<string[]>;
+		}
+	}
+}
 
 let mongo: any;
-beforeAll(async()=>{
-  process.env.JWT_KEY = 'a-temp-key-for-test';
+beforeAll(async () => {
+	process.env.JWT_KEY = 'a-temp-key-for-test';
 
-  mongo = new MongoMemoryServer();
-  const mongoUri = await mongo.getUri();
+	mongo = new MongoMemoryServer();
+	const mongoUri = await mongo.getUri();
 
-  await mongoose.connect(mongoUri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-
-  })
+	await mongoose.connect(mongoUri, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	});
 });
 
-beforeEach(async()=>{
-  const collections = await mongoose.connection.db.collections();
-  for(let collection of collections){
-    await collection.deleteMany({});
-  }
+beforeEach(async () => {
+	const collections = await mongoose.connection.db.collections();
+	for (let collection of collections) {
+		await collection.deleteMany({});
+	}
 });
 
-afterAll(async()=>{
-  await mongo.stop();
-  await mongoose.connection.close();
+afterAll(async () => {
+	await mongo.stop();
+	await mongoose.connection.close();
 });
 
+global.signup = async (email: string, password?: string) => {
+	const response = await request(app)
+		.post('/api/users/signup')
+		.send({
+			email,
+			password: password || 'test',
+		})
+		.expect(201);
+	return response.get('Set-Cookie');
+};
