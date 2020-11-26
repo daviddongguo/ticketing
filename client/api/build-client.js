898885cd726eaecd71b1b83a151a914215dfab9b
daviddongguo <david.dong.guo@gmail.com>
2020-11-26 09:44:45 -0500

    Use process.env to select baseUrl automatically

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,14 +4,16 @@ const buildClient = ({req}) => {
 	if (typeof window === 'undefined') {
 		// we are on the server!
 		return axios.create({
-			baseURL:
-				'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+			baseURL: process.env.serverBaseUrl
+				? process.env.serverBaseUrl
+				: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
 			headers: req.headers,
 		});
 	} else {
 		// we are on the browser!
 		return axios.create({
 			baseURL: '',
+			// baseURL: 'http://localhost:3018',
 		});
 	}
 };
