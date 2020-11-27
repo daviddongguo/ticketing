2c74c1bb25e4f840fd345b78904c32ee2b1c1b5a
daviddongguo <david.dong.guo@gmail.com>
2020-11-27 16:09:25 -0500

    Add log information in test

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -13,27 +13,44 @@ declare global {
 
 let mongo: any;
 beforeAll(async () => {
+  console.log('Test Starting....');
 	process.env.JWT_KEY = 'a-temp-key-for-test';
 
 	mongo = new MongoMemoryServer();
 	const mongoUri = await mongo.getUri();
 
-	await mongoose.connect(mongoUri, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	});
+	try {
+		await mongoose.connect(mongoUri, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		});
+		console.log('mongoDb connected.');
+	} catch (error) {
+		console.error(error);
+	}
 });
 
 beforeEach(async () => {
 	const collections = await mongoose.connection.db.collections();
 	for (let collection of collections) {
 		await collection.deleteMany({});
-	}
+  }
+  console.log("mongoDb cleaned.")
 });
 
 afterAll(async () => {
-	await mongo.stop();
-	await mongoose.connection.close();
+  if(!mongo || !mongoose){
+    return;
+  }
+  try {
+    await mongo.stop();
+    await mongoose.connection.close();
+    console.log("mongoDb Closed");
+  } catch (error) {
+    console.error(error);
+  }
+
+
 });
 
 global.signup = async (email: string, password?: string) => {
