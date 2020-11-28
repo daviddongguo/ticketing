816ecba76270b046d7a9388e3d4750e6a1149b52
daviddongguo <david.dong.guo@gmail.com>
2020-11-28 15:57:56 -0500

    Fake a sign in process

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -1,11 +1,14 @@
 import 'express-async-errors';
+import jwt from 'jsonwebtoken';
 import {MongoMemoryServer} from 'mongodb-memory-server';
 import mongoose from 'mongoose';
+
 
 declare global {
 	namespace NodeJS {
 		interface Global {
 			signup(email: string): Promise<string[]>;
+			signin(email: string): Promise<string[]>;
 		}
 	}
 }
@@ -21,7 +24,7 @@ beforeAll(async () => {
 		await mongoose.connect(mongoUri, {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
-		});
+    });
 	} catch (error) {
 		console.error(error);
 	}
@@ -48,13 +51,27 @@ afterAll(async () => {
 
 });
 
-// global.signup = async (email: string, password?: string) => {
-// 	const response = await request(app)
-// 		.post('/api/users/signup')
-// 		.send({
-// 			email,
-// 			password: password || 'test',
-// 		})
-// 		.expect(201);
-// 	return response.get('Set-Cookie');
-// };
+global.signup = async (email: string, password?: string) => {
+  // Build a JWT payload. {id, email}
+  const payload = {id: '5fc1b60998119500225995d7', email};
+
+  // Create the JWT
+  const accessToken  = jwt.sign(payload, process.env.JWT_KEY!);
+
+  // Build success Object. {jwt: MY_JWT}
+  const session = {jwt: accessToken };
+
+  //  Turn that session into JSON
+  const sessionJSON = JSON.stringify(session);
+
+  // Take JSON and encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString('base64');
+
+  // Return a string that is the cookie with the encoded data
+  return [`session=${base64};`];
+};
+
+global.signin = async (email: string, password?: string) => {
+  // session=eyJqd3QiOiJleUpoYkdjaU9pSklVekkxTmlJc0luUjVjQ0k2SWtwWFZDSjkuZXlKcFpDSTZJalZtWXpKaU9HVm1Zemt5WWpZM05HVmxPRE14WWpOa015SXNJbVZ0WVdsc0lqb2lkR1Z6ZEVCbGJXRnBiQzVqYjIwaUxDSnBZWFFpT2pFMk1EWTFPVFk0TkRkOS5YU0FMTWw3YTNKc185ZFpBX2t2U25hX2F0TnFZb0hnRmg2cHdBcmo1c3ZjIn0=; path=/; expires=Sun, 29 Nov 2020 20:54:07 GMT; httponly
+	return await global.signup(email);
+};
