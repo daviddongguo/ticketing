827be2cc26e8158dae9ef528cb77ae6fa41c196f
daviddongguo <david.dong.guo@gmail.com>
2020-11-27 21:11:24 -0500

    Upgrade mongo uri

---
+++ tickets/configs/mongoDb.ts
@@ -0,0 +1,5 @@
+module.exports = {
+	localDb:
+    'mongodb://127.0.0.1/tickets',
+  googleDb: process.env.MONGO_URI
+};
--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -4,18 +4,22 @@ import {app} from './app';
 const mongoDbString = require('../configs/mongoDb');
 
 const start = async () => {
+  let connectionString = '';
   if(process.env.NODE_ENV === 'local'){
+    connectionString = mongoDbString.localDb;
     process.env.JWT_KEY = "local-jwt-key";
-  }
-  if(!process.env.JWT_KEY){
-    throw new Error('JWT_KEY must be defined.')
+  }else{
+    if(!mongoDbString.googleDb){
+      throw new Error('MONGO_URI must be defined.')
+    }
+    connectionString = mongoDbString.googleDb;
+
+    if(!process.env.JWT_KEY){
+      throw new Error('JWT_KEY must be defined.')
+    }
   }
 
 	try {
-		let connectionString = mongoDbString.googleDb;
-		if (process.env.NODE_ENV === 'local') {
-			connectionString = mongoDbString.localDb;
-		}
 
 		await mongoose.connect(connectionString, {
 			useNewUrlParser: true,
