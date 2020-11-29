704f086364f1e501c84ba241c3cecacb800f8e36
daviddongguo <david.dong.guo@gmail.com>
2020-11-28 22:03:19 -0500

    Update user model

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -9,18 +9,20 @@ interface UserAttrs {
 }
 
 // An interface that describes the properties
-// that a User Model has
-interface UserModel extends mongoose.Model<UserDoc> {
-	build(attrs: UserAttrs): UserDoc;
-}
-
-// An interface that describes the properties
 // that a User Document has
 interface UserDoc extends mongoose.Document {
   id: string;
   email: string;
 	password: string;
 }
+
+// An interface that describes the properties
+// that a User Model has
+interface UserModel extends mongoose.Model<UserDoc> {
+	build(attrs: UserAttrs): UserDoc;
+}
+
+
 
 const userSchema = new mongoose.Schema(
 	{
