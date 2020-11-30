ea1ad1951632171055c65ca84a02cdb96f931d9e
daviddongguo <david.dong.guo@gmail.com>
2020-11-29 20:31:37 -0500

    Add show tickets

--- tickets/src/routers/show.ts
+++ tickets/src/routers/show.ts
@@ -1,34 +1,36 @@
+import {
+  BadRequestError,
+  DatabaseConnectionError,
+  NotFoundError
+} from '@davidgarden/common';
 import express, {Request, Response} from 'express';
-import {Ticket} from '../models/ticket';
-import {DatabaseConnectionError} from './../../../common/src/errors/database-connection-error';
-import {NotFoundError} from './../../../common/src/errors/not-found-error';
+import {Ticket, TicketDoc} from '../models/ticket';
+const mongoose = require('mongoose');
 
 const router = express.Router();
 
-router.get('/api/tickets', async (req: Request, res: Response) => {
-	let query = {};
-
-	try {
-		const tickets = await Ticket.find(query);
-		return res.status(200).send({tickets});
-	} catch (error) {
-		throw new DatabaseConnectionError('Broke! as querying ticket');
-	}
-});
 router.get('/api/tickets/:ticketId', async (req: Request, res: Response) => {
-	const ticketId = req.params.ticketId;
-	if (!ticketId) {
-    throw new NotFoundError(`id=${ticketId} not found.`);
-	}
-	const query = {_id: ticketId};
+  const ticketId = req.params.ticketId;
 
+  // returns 400 if error
+	if (!mongoose.Types.ObjectId.isValid(ticketId)) {
+		throw new BadRequestError(`${ticketId} is not a valid mongoose objectId`);
+  }
+
+  // returns 400 if error
+	let ticket: TicketDoc | null = null;
 	try {
-    const ticket = await Ticket.findOne(query);
-    const tickets = [ticket];
-		return res.status(200).send({tickets});
+	ticket = await Ticket.findById(ticketId).maxTimeMS(200).exec();
 	} catch (error) {
-		throw new DatabaseConnectionError('Broke! as querying ticket');
+		throw new DatabaseConnectionError(error.message);
+  }
+
+  // returns 404 if error
+	if (!ticket) {
+		throw new NotFoundError(`id=${ticketId}`);
 	}
+
+	return res.status(200).send({tickets: [ticket]});
 });
 
 export {router as showTicketsRouter};
