5e4f80e4d6451c3035a6f6ac9a3eda947e0a458f
daviddongguo <david.dong.guo@gmail.com>
2020-11-30 22:55:42 -0500

    Add manual ack mode

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -6,22 +6,27 @@ const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
 	url: 'http://35.196.98.224:4222',
 });
 
-stan.on('connect', ()=>{
-  console.log('Listener connected to NATS');
+stan.on('connect', () => {
+	console.log('Listener connected to NATS');
 
-  const subscription = stan.subscribe('ticket:created', 'orders-service-queue-group');
+	const options = stan.subscriptionOptions().setManualAckMode(true);
+	const subscription = stan.subscribe(
+		'ticket:created',
+		'orders-service-queue-group',
+		options
+	);
 
-  subscription.on('message', (msg: Message)=>{
-    // console.log('Message received.');
-    // console.log(msg.getSubject());
-    // console.log(msg.getSequence());
-    // console.log(msg.getData());
+	subscription.on('message', (msg: Message) => {
+		// console.log('Message received.');
+		// console.log(msg.getSubject());
+		// console.log(msg.getSequence());
+		// console.log(msg.getData());
 
-    const data = msg.getData();
-    if(typeof data === 'string'){
-      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
+		const data = msg.getData();
+		if (typeof data === 'string') {
+			console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
     }
-  });
 
-
+    msg.ack();
+	});
 });
