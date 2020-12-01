3c22dfc78091bf328463a66b489ac0819d033db5
daviddongguo <david.dong.guo@gmail.com>
2020-12-01 11:01:19 -0500

    Test setdeliverAllAvailabe setDurableName and queue-group-name three options

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -7,14 +7,18 @@ const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
 });
 
 stan.on('connect', () => {
-  console.log('Listener connected to NATS');
+	console.log('Listener connected to NATS');
 
-  stan.on('close', ()=>{
-    console.log('Nats connection closed!');
-    process.exit();
-  });
+	stan.on('close', () => {
+		console.log('Nats connection closed!');
+		process.exit();
+	});
 
-	const options = stan.subscriptionOptions().setManualAckMode(true);
+	const options = stan
+		.subscriptionOptions()
+		.setManualAckMode(true)
+    .setDeliverAllAvailable()
+    .setDurableName('accounting-service');
 	const subscription = stan.subscribe(
 		'ticket:created',
 		'orders-service-queue-group',
@@ -30,11 +34,11 @@ stan.on('connect', () => {
 		const data = msg.getData();
 		if (typeof data === 'string') {
 			console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
-    }
+		}
 
-    msg.ack();
+		msg.ack();
 	});
 });
 
-process.on('SIGINT', ()=> stan.close());
-process.on('SIGTERM', ()=> stan.close());
+process.on('SIGINT', () => stan.close());
+process.on('SIGTERM', () => stan.close());
