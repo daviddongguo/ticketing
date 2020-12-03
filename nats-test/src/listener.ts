16622c71f6280aa516054b2f99b42efad670a80a
daviddongguo <david.dong.guo@gmail.com>
2020-12-02 20:44:45 -0500

    Find bug

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,5 +1,7 @@
 import {randomBytes} from 'crypto';
-import nats, {Message} from 'node-nats-streaming';
+import nats from 'node-nats-streaming';
+import {TicketCreatedListener} from './events/ticket-created-listener';
+
 console.clear();
 
 const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
@@ -14,30 +16,7 @@ stan.on('connect', () => {
 		process.exit();
 	});
 
-	const options = stan
-		.subscriptionOptions()
-		.setManualAckMode(true)
-    .setDeliverAllAvailable()
-    .setDurableName('accounting-service');
-	const subscription = stan.subscribe(
-		'ticket:created',
-		'orders-service-queue-group',
-		options
-	);
-
-	subscription.on('message', (msg: Message) => {
-		// console.log('Message received.');
-		// console.log(msg.getSubject());
-		// console.log(msg.getSequence());
-		// console.log(msg.getData());
-
-		const data = msg.getData();
-		if (typeof data === 'string') {
-			console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
-		}
-
-		msg.ack();
-	});
+	new TicketCreatedListener(stan).listen();
 });
 
 process.on('SIGINT', () => stan.close());
