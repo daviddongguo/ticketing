7c4b1572e3da1834fca80aad5fc3aef961f72f95
daviddongguo <david.dong.guo@gmail.com>
2020-12-04 20:37:18 -0500

    Add validate for new method

--- orders/src/routers/new.ts
+++ orders/src/routers/new.ts
@@ -1,34 +1,39 @@
-import {BadRequestError, DatabaseConnectionError, requireAuth} from '@davidgarden/common';
+import {
+  DatabaseConnectionError,
+  requireAuth
+} from '@davidgarden/common';
 import express, {Request, Response} from 'express';
+import {body} from 'express-validator';
 import mongoose from 'mongoose';
 import {Order} from '../models/order';
+import {validateRequest} from './../../../common/src/middlewares/validate-request';
 import {OrderCreatedPublisher} from './../events/publishers/order-created-publisher';
 import {natsWrapper} from './../nats-wrapper';
 const router = express.Router();
 
-
 router.post(
 	'/api/orders',
 	requireAuth, // Throw Error if currentUser is null
+  [body('ticketId')
+  .custom((input: string)=> mongoose.Types.ObjectId.isValid(input))
+  .withMessage('TicketId must be provided.')],
+	validateRequest,
 	async (req: Request, res: Response) => {
-    const {ticketId} = req.body;
-    if(!mongoose.Types.ObjectId.isValid(ticketId)){
-      throw new BadRequestError(`ticketId = ${ticketId} is not valid`);
-    }
-    const userId = req.currentUser!.id;
-    const status = 'pending';
-    const expiresAt = 'a time string';
+		const {ticketId} = req.body;
+		const userId = req.currentUser!.id;
+		const status = 'pending';
+		const expiresAt = 'a time string';
 		try {
-      // save doc to DB and push event to NATS
-      const newOrder = Order.build({ticketId, userId, status, expiresAt});
-      await newOrder.save();
-      await new OrderCreatedPublisher(natsWrapper.client).publish({
-        id: newOrder.id,
-        ticketId: newOrder.ticketId,
-        userId: newOrder.userId,
-        status: newOrder.status,
-        expiresAt: newOrder.expiresAt,
-      });
+			// save doc to DB and push event to NATS
+			const newOrder = Order.build({ticketId, userId, status, expiresAt});
+			await newOrder.save();
+			await new OrderCreatedPublisher(natsWrapper.client).publish({
+				id: newOrder.id,
+				ticketId: newOrder.ticketId,
+				userId: newOrder.userId,
+				status: newOrder.status,
+				expiresAt: newOrder.expiresAt,
+			});
 			return res.status(201).send(newOrder);
 		} catch (error) {
 			throw new DatabaseConnectionError('Broke! as saving order');
