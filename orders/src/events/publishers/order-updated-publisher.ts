e41761f588521ccbd8a630e7f90c0f1d14813f77
daviddongguo <david.dong.guo@gmail.com>
2020-12-05 16:47:56 -0500

    Delete update in orders service

--- orders/src/events/publishers/order-updated-publisher.ts
+++
@@ -1,5 +0,0 @@
-import {OrderUpdatedEvent, Publisher, Subjects} from '@davidgarden/common';
-
-export class OrderUpdatedPublisher extends Publisher<OrderUpdatedEvent> {
-	readonly subject = Subjects.OrderUpdated;
-}
