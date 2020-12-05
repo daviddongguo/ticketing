2a6b3b774e6c87f23c7d3eadf5b0b7aaa0bccfde
daviddongguo <david.dong.guo@gmail.com>
2020-12-05 11:15:18 -0500

    Upgrade index test

--- orders/src/routers/__test__/index.test.ts
+++ orders/src/routers/__test__/index.test.ts
@@ -13,14 +13,14 @@ it(`retrieves a list of orders to ${baseUrl} for get requests`, async () => {
   // Create three tickets
   const ticket01 = await global.createTicket();
   const ticket02 = await global.createTicket();
-  const ticket = await global.createTicket();
+  const ticket03 = await global.createTicket();
 
   // Create one order as User #1
-  await global.createOrder(ticket02, global.secondUserId);
+  await global.createOrder(ticket01, global.secondUserId);
 
   // Create two order as User #2
-  await global.createOrder(ticket01, global.userId);
-  await global.createOrder(ticket, global.userId);
+  await global.createOrder(ticket02, global.userId);
+  await global.createOrder(ticket03, global.userId);
 
   // Make request to get orders for User #2
   const response = await request(app).get(baseUrl).set('Cookie', global.cookie);
@@ -28,8 +28,8 @@ it(`retrieves a list of orders to ${baseUrl} for get requests`, async () => {
   // Make sure we only got the orders for User #2
 	expect(response.status).toEqual(200);
   expect(response.body.orders.length).toEqual(2);
-  expect(response.body.orders[0].ticket.id).toEqual(ticket01.id);
-  expect(response.body.orders[1].ticket.id).toEqual(ticket.id);
+  expect(response.body.orders[0].ticket.id).toEqual(ticket02.id);
+  expect(response.body.orders[1].ticket.id).toEqual(ticket03.id);
 });
 
 
