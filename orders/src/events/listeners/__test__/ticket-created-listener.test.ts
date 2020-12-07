9bef71ebac74d429390c386431a167d4ff3971cc
daviddongguo <david.dong.guo@gmail.com>
2020-12-06 20:24:36 -0500

    Setup a test for ticket created listener

---
+++ orders/src/events/listeners/__test__/ticket-created-listener.test.ts
@@ -0,0 +1,17 @@
+const setup = async () => {
+	// create an instance of the listener
+	// create a fake data event
+	// create a fake message object
+};
+
+it('creats and saves a ticket', async () => {
+  await setup();
+	// call the onMessage function with the data object + message objec
+	// write assertions to make sure a ticket was created.
+});
+
+it('acks the message', async () => {
+  await setup();
+  // call the onMessage function with the data object + message objec
+  // write assertions to make sure ack function is called.
+});
