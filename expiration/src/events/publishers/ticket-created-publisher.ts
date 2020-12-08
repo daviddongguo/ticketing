5539246b7157ee44d7a38e2a1aea8ee8c5022d0e
daviddongguo <david.dong.guo@gmail.com>
2020-12-08 08:59:31 -0500

    Add publish for expiration server

--- expiration/src/events/publishers/ticket-created-publisher.ts
+++
@@ -1,6 +0,0 @@
-
-import {Publisher, Subjects, TicketCreatedEvent} from '@davidgarden/common';
-
-export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
-	readonly subject = Subjects.TicketCreated;
-}
