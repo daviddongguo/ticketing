3f9906c67a53d0b9a4c674fa425e73e8514085d4
daviddongguo <david.dong.guo@gmail.com>
2020-12-10 00:26:51 -0500

    Add mock

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -3,9 +3,14 @@ import mongoose from 'mongoose';
 import request from 'supertest';
 import {app} from '../../app';
 import {Order} from '../../models/order';
+import {stripe} from '../../stripe';
+
+jest.mock('../../stripe');  // not the real stripe
 
 const url = '/api/payments';
 const userId = mongoose.Types.ObjectId().toHexString();
+const token= 'tok_mastercard';
+const price = 1.99;
 const cookie = global.signup(userId);
 const buildOrder = () => {
 	return Order.build({
@@ -13,7 +18,7 @@ const buildOrder = () => {
 		status: OrderStatus.Created,
 		version: 0,
 		userId,
-		price: 1.91,
+		price,
 	});
 };
 
@@ -21,7 +26,20 @@ it('returns success', async () => {
 	const order = await buildOrder().save();
 	const response = await request(app).post(url).set('Cookie', cookie).send({
 		orderId: order.id,
-		token: 'tok_mastercard',
+		token,
+	});
+  expect(response.status).toEqual(201);
+
+  const chargeOptions = (stripe.charges.create as jest.Mock).mock.calls[0][0];
+  expect(chargeOptions.source).toEqual(token);
+  expect(chargeOptions.amount).toEqual(price * 100);
+  expect(chargeOptions.currency).toEqual('cad');
+});
+it('returns a 204 with valid inputs', async () => {
+	const order = await buildOrder().save();
+	const response = await request(app).post(url).set('Cookie', cookie).send({
+		orderId: order.id,
+		token,
 	});
 	expect(response.status).toEqual(201);
 });
@@ -30,7 +48,7 @@ it('returns a 404 when purchasing an order that does not exits', async () => {
 	// await order.save();
 	const response = await request(app).post(url).set('Cookie', cookie).send({
 		orderId: mongoose.Types.ObjectId().toHexString(),
-		token: 'tok_mastercard',
+		token,
 	});
 	expect(response.status).toEqual(404);
 });
@@ -42,7 +60,7 @@ it('returns a 401 when purchasing an order that doesnt belong to the user', asyn
 		.set('Cookie', global.signin()) // new user created
 		.send({
 			orderId: order.id,
-			token: 'tok_mastercard',
+			token,
 		});
 	expect(response.status).toEqual(401);
 });
@@ -53,7 +71,7 @@ it('returns a 400 when purchasing a cancelled order', async () => {
 	await order.save();
 	const response = await request(app).post(url).set('Cookie', cookie).send({
 		orderId: order.id,
-		token: 'tok_mastercard',
+		token,
 	});
 	expect(response.status).toEqual(400);
 });
