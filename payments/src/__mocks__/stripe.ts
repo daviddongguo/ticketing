4eff136e95ba44038a527e5f2fbbcde72aeee58b
daviddongguo <david.dong.guo@gmail.com>
2020-12-10 19:39:33 -0500

    Upgrade strip mock to create an id

--- payments/src/__mocks__/stripe.ts
+++ payments/src/__mocks__/stripe.ts
@@ -1,6 +1,19 @@
-
+export const STRIPE_ID = 'a-stripe-id';
 export const stripe = {
-  charges: {
-    create: jest.fn().mockResolvedValue({}),
-  }
+	charges: {
+		create: jest
+			.fn()
+			.mockImplementation(
+				(
+					amount: number,
+					currency: string,
+					source: string,
+					descript: string
+				) => {
+					return new Promise(function (Resolve, Reject) {
+						Resolve({id: STRIPE_ID});
+					});
+				}
+			),
+	},
 };
