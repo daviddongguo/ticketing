ccf23a407cbea66b0faacf0d080d487cb05d51b7
daviddongguo <david.dong.guo@gmail.com>
2020-12-10 21:30:45 -0500

    upgrade cors

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -11,7 +11,7 @@ import {createOrderRouter} from './routers/new';
 import {showOrdersRouter} from './routers/show';
 
 const app = express();
-app.use(cors({ credentials: true }));
+app.use(cors);
 // traffic is being proximate to our application through ingress engine express
 app.set('trust proxy', true); //  the X-Forwarded-* header fields may be trusted
 app.use(json());
