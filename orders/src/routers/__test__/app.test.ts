e7846d110edaf71ce62133da7378df6a2b5b14b6
daviddongguo <david.dong.guo@gmail.com>
2020-12-11 11:16:11 -0500

    Redescribe app test

--- orders/src/routers/__test__/app.test.ts
+++ orders/src/routers/__test__/app.test.ts
@@ -1,13 +1,13 @@
 import request from 'supertest';
 import {app} from '../../app';
 
-it('Returns 404 statusCode to /no-existed-router', async ()=>{
+it('Route to no-existed-router', async ()=>{
   const response = await request(app).post('/api/users/no-existed-router').send({});
 
   expect(response.status).toEqual(404);
 });
 
-it('Returns 200 statusCode and a message to /api/users/test', async ()=>{
+it('Route to api/users/test', async ()=>{
   const response = await request(app).get('/api/users/test');
 
   expect(response.status).toEqual(200);
